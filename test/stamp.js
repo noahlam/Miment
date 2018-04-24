4df369d1b8073acd32b55a5b7aa5ad70b4b8ffc8
noahlam <284516433@qq.com>
2018-04-24 09:42:21 +0800

    chore: fix lint warn

--- test/stamp.js
+++ test/stamp.js
@@ -3,7 +3,7 @@ import test from 'ava'
 import miment from '../src/miment'
 
 // it's not exact before support time zone
-// test('stamp api', t => {
-//   const m = miment('2008-02-21 12:34:56')
-//   t.deepEqual(m.stamp(), 1203568496000)
-// })
+test('stamp api', t => {
+  const m = miment('2008-02-21 12:34:56')
+  t.deepEqual(typeof m.stamp(), 'number')
+})
